dcf156ae9ca77b8fd3fc5c2538560182ffd86e3c
adammc331 <amcneilly331@gmail.com>
2019-01-21 16:05:20 -0500

    Added random functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_xkcd/data/api.dart';
 import 'package:flutter_xkcd/data/database.dart';
@@ -31,11 +33,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  XKCDRepository _getRepository() {
-    return XKCDRepository(
-      api: XKCDApi(),
-      database: XKCDDatabase.db,
-    );
+  final _repository = XKCDRepository(
+    api: XKCDApi(),
+    database: XKCDDatabase.db,
+  );
+
+  final _pageController = PageController(
+    initialPage: 0,
+  );
+
+  int _itemCount = 0;
+
+  void _onRandomPressed() {
+    _pageController.jumpToPage(Random().nextInt(_itemCount));
   }
 
   @override
@@ -43,6 +53,17 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: _onRandomPressed,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text('RANDOM'),
+              ),
+            ),
+          ),
+        ],
       ),
       body: SafeArea(
         child: _buildComicPager(),
@@ -54,27 +75,27 @@ class _MyHomePageState extends State<MyHomePage> {
   /// comic number, and then paginates through them.
   Widget _buildComicPager() {
     return FutureBuilder<Comic>(
-        future: _getRepository().fetchLatestComic(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return PageView.builder(
-              controller: PageController(
-                initialPage: 0,
-              ),
-              itemBuilder: (context, index) {
-                return ComicPage(
-                  repository: _getRepository(),
-                  comicNumber: snapshot.data.number - index,
-                );
-              },
-              itemCount: snapshot.data.number,
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('${snapshot.error}'));
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      );
+      future: _repository.fetchLatestComic(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          _itemCount = snapshot.data.number;
+
+          return PageView.builder(
+            controller: _pageController,
+            itemBuilder: (context, index) {
+              return ComicPage(
+                repository: _repository,
+                comicNumber: snapshot.data.number - index,
+              );
+            },
+            itemCount: snapshot.data.number,
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text('${snapshot.error}'));
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
